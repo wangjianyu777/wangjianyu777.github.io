db97ff66d02d491498130de13a83cda732a59515
wangjianyu777 <jianyu0820@gmail.com>
2022-12-23 07:30:37 +0800

    1223

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,5 @@
 let myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+myHeading.textContent = "Hello ";
 function toggle_info() {
   const foo = document.getElementById("foo");
   // 取得 `foo` 的 class 列表。
